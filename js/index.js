f1c2b9095ad869707834a2b4b160a0939b46f4fe
xineman <uragupalo@mail.ru>
2017-08-13 17:23:35 +0300

    desktop version

---
+++ js/index.js
@@ -0,0 +1,53 @@
+$(function() {
+  $(".top-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    dotsClass: "top-slider__dots container",
+    draggable: false
+  });
+  $(document).scroll(function() {
+    if ($(document).scrollTop()) {
+      $(".header").addClass("header_fixed");
+    } else {
+      $(".header").removeClass("header_fixed");
+    }
+  });
+  var triggersSlider = false;
+  $(window).resize(function() {
+    if (window.innerWidth<=1170 && !triggersSlider) {
+      $(".top__triggers").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        dots: true,
+        dotsClass: "top__triggers__dots",
+        draggable: false
+      });
+      triggersSlider = true;
+    } else if (window.innerWidth>1170 && triggersSlider) {
+      $(".top__triggers").slick("unslick");
+      triggersSlider = false;
+    }
+  });
+  if (window.innerWidth<=1170) {
+    $(".top__triggers").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      dots: true,
+      dotsClass: "top__triggers__dots",
+      draggable: false
+    });
+    triggersSlider = true;
+  }
+  $(".header__burger").click(function() {
+    $(".header__menu-wrapper").addClass("active");
+  });
+  $(".header__menu-close").click(function() {
+    $(".header__menu-wrapper").removeClass("active");
+  });
+});
